d875c4f35f5ba43e4e69d514e98c3abe62370754
GabrielGiliotti <gabrielgiliotti@hotmail.com>
2024-01-22 11:45:39 -0300

    Organizacao dos dados para calcular metricas

---
+++ Backend/src/client.model.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+export class ClientModel {
+    'quantidade cobranças': string
+    'cobrada a cada X dias': string
+    'data início': string
+    status: string
+    'data status': string
+    'data cancelamento': string
+    valor: string
+    'próximo ciclo': string
+    'ID assinante': string
+} 
--- Backend/src/metricas.controller.ts
+++ Backend/src/metricas.controller.ts
@@ -22,44 +22,7 @@ export class MetricasController {
         
         this.fileRepository.saveFile(file);
 
-        const csv = file.buffer.toString();
-        const array = csv.toString().split("\r");
-        const result = [];
-        const headers = array[0].split(", ")
-        
-        for (let i = 1; i < array.length; i++) {
-            const obj = {}
-        
-            const str = array[i]
-            let s = ''
-            let flag = 0
-
-            for (let ch of str) {
-                if (ch === '"' && flag === 0) {
-                    flag = 1
-                }
-                else if (ch === '"' && flag == 1) flag = 0
-                if (ch === ', ' && flag === 0) ch = '|'
-                if (ch !== '"') s += ch
-            }
-        
-            const properties = s.split("|")
-           
-            for (const j in headers) {
-                const array1 = headers[j].split(",");
-                const array2 = properties[j].split(",");
-
-                array2[0] = array2[0].split("\n")[1];
-                array2[6] = array2[6] + ',' + array2[7];
-                array2.splice(7,1);
-
-                for(let f = 0; f < array1.length; f++) {
-                    obj[array1[f]] = array2[f];
-                };
-            }
-        
-            result.push(obj)
-        }
+        const result = await this.fileRepository.buildJson(file.buffer);
 
         return {message: 'Arquivo salvo com sucesso na memoria', file: result};
     }
@@ -67,7 +30,12 @@ export class MetricasController {
     @Get('mrr')
     async metricaMRR() {
 
-        return this.fileRepository.getFile();
+        const array = await this.fileRepository.getJson();
+        const sorted = await this.fileRepository.sortArray(array);
+        const formatted = await this.fileRepository.formatArrayDate(sorted, 'data início');
+        const arrayMonth = await this.fileRepository.formatDataByMonth(formatted);
+
+        return arrayMonth;
     }
 
     @Get('churn-rate')
